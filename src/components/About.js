2285a8034e4e8d584ea520821a5bb1ae801297a6
Abinash <abinash20001010@gmail.com>
2021-12-09 19:33:17 +0545

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div className="container">
+      <h4>This is About</h4>
+      <p>
+        How can you convince men to delve into skincare? You bring their best
+        friend on their side – a dog. Meet Bulldog Skincare, the company that
+        aims to make skincare options dedicated to men readily available. To
+        have you sold, their About Us page greets you with the adorable mug of a
+        bulldog. This quickly becomes your guide to finding the products that
+        will help you look and feel your best. As you scroll down, you are
+        growing familiar with the products and are slowly but surely befriending
+        the brand itself and its values.
+      </p>
+    </div>
+  );
+};
